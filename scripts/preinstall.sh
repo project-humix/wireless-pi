d9e631d78e76d7a8b4349997fbeceb7bda4c39c8
yhwang <yihongwang@gmail.com>
2016-10-12 16:21:06 +0000

    modify the preinstall script
    no need to download/build the special driver
    for edmax. use the built-in wireless in pi3
    instead.

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-sudo apt-get update
-sudo apt-get install bridge-utils hostapd isc-dhcp-server
+apt-get update
+apt-get install bridge-utils hostapd isc-dhcp-server
 
-if [ ! -d deps ]; then
-  mkdir deps
-fi
+#if [ ! -d deps ]; then
+#  mkdir deps
+#fi
 
-wget http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/cn/wlan/0001-RTL8188C_8192C_USB_linux_v4.0.2_9000.20130911.zip -O deps/driver.zip
-unzip deps/driver.zip -d deps
-tar zxf deps/RTL8188C*/wpa_supplicant_hostapd/wpa_supplicant_hostapd*.tar.gz -C deps/RTL8188C*/wpa_supplicant_hostapd/
-sudo make install -C deps/RTL8188C*/wpa_supplicant_hostapd/wpa_supplicant_hostapd*/hostapd
+#wget http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/cn/wlan/0001-RTL8188C_8192C_USB_linux_v4.0.2_9000.20130911.zip -O deps/driver.zip
+#unzip deps/driver.zip -d deps
+#tar zxf deps/RTL8188C*/wpa_supplicant_hostapd/wpa_supplicant_hostapd*.tar.gz -C deps/RTL8188C*/wpa_supplicant_hostapd/
+#sudo make install -C deps/RTL8188C*/wpa_supplicant_hostapd/wpa_supplicant_hostapd*/hostapd
